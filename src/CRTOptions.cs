26f1a7deca88f4d52fefb9cd4bb3d8207e925075
ASlightlyOvergrownCactus <82615439+ASlightlyOvergrownCactus@users.noreply.github.com>
2024-03-03 12:01:47 -0600

    Palette Update!
    Adds a new posterization shader that is toggleable, with 31 palettes to choose from! Custom palettes will come soon....

--- src/CRTOptions.cs
+++ src/CRTOptions.cs
@@ -1,4 +1,5 @@
-﻿using BepInEx.Logging;
+﻿using System.Collections.Generic;
+using BepInEx.Logging;
 using JetBrains.Annotations;
 using Menu.Remix.MixedUI;
 using Menu.Remix.MixedUI.ValueTypes;
@@ -17,16 +18,73 @@ public class CRTOptions : OptionInterface
     public static float distortionF;
     public static Configurable<bool> screenDist;
     public static bool screenDistF;
+    
+    public static Configurable<float> brightness;
+    public static float brightnessF;
+    public static Configurable<float> offset;
+    public static float offsetF;
+    public static Configurable<float> contrast;
+    public static float contrastF;
+    public static Configurable<string> dither;
+    public static string ditherF;
+    public static Configurable<string> palette;
+    public static string paletteF;
+    public static Configurable<bool> usePal;
+    public static bool usePalF;
+
+    // Most palettes here were gotten from https://lospec.com/palette-list / They have a lot of great palettes! Go check em out!
+    public static List<ListItem> palettes = new List<ListItem>()
+    {
+        // 4-color palettes
+        new ("Chrome4"),
+        new ("Hollow4"),
+        new ("RetroGB4"),
+        new ("Jojo4"),
+        new ("Amber4"),
+        new ("Blood4"),
+        new ("Horror4"),
+        new ("Lava4"),
+        new ("Miku4"),
+        new ("Aqua4"),
+        new ("Wish4"),
+        new ("Moonlight4"),
+        new ("Royal4"),
+        // 8-color palettes
+        new ("Ammo8"),
+        new ("Bi8"),
+        new ("Borkfest8"),
+        new ("Citrink8"),
+        new ("Dream8"),
+        new ("Fox8"),
+        new ("Gothic8"),
+        new ("Lava8"),
+        new ("Morning8"),
+        new ("Nebula8"),
+        new ("Ocean8"),
+        new ("Paper8"),
+        new ("Parchment8"),
+        new ("Purple8"),
+        new ("RetroGB8"),
+        new ("RustGold8"),
+        new ("Chimera8"),
+        new ("Winter8")
+    };
 
     [CanBeNull]
     public static UIelement[] uIelements;
 
     public CRTOptions()
     {
-        intensity = config.Bind<float>("intensity", 25, new ConfigAcceptableRange<float>(0, 100));
-        scanLineDarkness = config.Bind<float>("scanLines", 50, new ConfigAcceptableRange<float>(0, 100));
-        distortion = config.Bind<float>("distortion", 10, new ConfigAcceptableRange<float>(0, 100));
-        screenDist = config.Bind<bool>("screenDist", true);
+        intensity = config.Bind<float>("CRTWorld_intensity", 25, new ConfigAcceptableRange<float>(0, 100));
+        scanLineDarkness = config.Bind<float>("CRTWorld_scanLines", 50, new ConfigAcceptableRange<float>(0, 100));
+        distortion = config.Bind<float>("CRTWorld_distortion", 10, new ConfigAcceptableRange<float>(0, 100));
+        screenDist = config.Bind<bool>("CRTWorld_screenDist", true);
+        brightness = config.Bind<float>("CRTWorld_brightness", 50, new ConfigAcceptableRange<float>(0, 100));
+        offset = config.Bind<float>("CRTWorld_offset", 25, new ConfigAcceptableRange<float>(0, 100));
+        contrast = config.Bind<float>("CRTWorld_contrast", 80, new ConfigAcceptableRange<float>(0, 100));
+        dither = config.Bind<string>("CRTWorld_dither", "Bayer16");
+        palette = config.Bind<string>("CRTWorld_palette", "Chrome4");
+        usePal = config.Bind<bool>("CRTWorld_usepal", true);
     }
 
     public override void Initialize()
@@ -58,7 +116,27 @@ public class CRTOptions : OptionInterface
             
             // Make the options on the left side
             new OpCheckBox(screenDist, new Vector2(leftSidePos, 520)) {description=Translate("Controls the full screen edge distortion")},
-            new OpLabel(leftSidePos+30, 523, Translate("CRT Screen Edge"))
+            new OpLabel(leftSidePos+30, 523, Translate("CRT Screen Edge")),
+            
+            new OpFloatSlider(brightness, new Vector2(leftSidePos, 440), sliderBarLength) {description=Translate("Brightness of gameboy lines.")},
+            new OpLabel(leftSidePos, 415, Translate("\nBrightness of gameboy grid. \nValues lower than 50 are darker, \nhigher than 50 are brighter.")),
+            
+            // put palette list here at y 260 label y 290 right
+            new OpComboBox(palette, new Vector2(rightSidePos, 180), 100, palettes),
+            new OpLabel(rightSidePos, 210, Translate("Preset Palettes to use.")),
+            
+            new OpCheckBox(usePal, new Vector2(rightSidePos, 280)) {description=Translate("Toggles the Posterization shader.")},
+            new OpLabel(rightSidePos+30, 260, Translate("Toggle Posterization.")),
+            
+            // put dither list here at y 340 label y 360 left
+            new OpListBox(dither, new Vector2(leftSidePos, 250), 100, new List<ListItem>{new ListItem("Bayer16"),new ListItem("Bayer8"),new ListItem("Bayer4"),new ListItem("Bayer2")}),
+            new OpLabel(leftSidePos, 370, Translate("Type of Bayer dither.")),
+            
+            new OpFloatSlider(offset, new Vector2(leftSidePos, 200), sliderBarLength) {description=Translate("Offset of palette.")},
+            new OpLabel(leftSidePos, 175, Translate("Offsets the palette.\nValues lower than 50 offset darker,\nhigher than 50 offset lighter")),
+            
+            new OpFloatSlider(contrast, new Vector2(leftSidePos, 120), sliderBarLength) {description=Translate("Contrast of palette shader.")},
+            new OpLabel(leftSidePos, 100, Translate("Contrast of the palette."))
         };
         opTab.AddItems(uIelements);
         
@@ -72,6 +150,12 @@ public class CRTOptions : OptionInterface
             scanLineDarknessF = ((OpFloatSlider)uIelements[3]).GetValueFloat();
             distortionF = ((OpFloatSlider)uIelements[5]).GetValueFloat();
             screenDistF = ((OpCheckBox)uIelements[7]).GetValueBool();
+            brightnessF = ((OpFloatSlider)uIelements[9]).GetValueFloat();
+            paletteF = ((OpComboBox)uIelements[11])._GetDisplayValue();
+            usePalF = ((OpCheckBox)uIelements[13]).GetValueBool();
+            ditherF = ((OpListBox)uIelements[15])._GetDisplayValue();
+            offsetF = ((OpFloatSlider)uIelements[17]).GetValueFloat();
+            contrastF = ((OpFloatSlider)uIelements[19]).GetValueFloat();
         }
     }
 }
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -19,11 +19,17 @@ using UnityEngine.PlayerLoop;
 
 namespace MerShaderLoader;
 
-[BepInPlugin("cactus.crt", "CRT", "1.0")]
+[BepInPlugin("cactus.crt", "CRT", "1.1")]
 sealed class Plugin : BaseUnityPlugin
 {
     static bool loaded = false;
     public static Shader CRTShader;
+    public static Shader GameShader;
+    public static Texture2D[] BayerTextures = new Texture2D[4];
+    public static List<Texture2D> palettes = new List<Texture2D>();
+    public static readonly int palNum = 31;
+
+    public static MaterialPropertyBlock bayerBlock;
     public void OnEnable()
     {
         try
@@ -71,7 +77,53 @@ sealed class Plugin : BaseUnityPlugin
         if (!loaded)
         {
             loaded = true;
-            var bundle = AssetBundle.LoadFromFile(AssetManager.ResolveFilePath("assets/crtscreen")); // Loads the asset bundle from your mod's file path
+            var bundle = AssetBundle.LoadFromFile(AssetManager.ResolveFilePath("assets/crtscreen", false)); // Loads the asset bundle from your mod's file path
+
+            bayerBlock = new();
+
+            // Dithering Textures
+            BayerTextures[0] = bundle.LoadAsset<Texture2D>("Assets/shaders 1.9.03/bayer2tile16.png");
+            BayerTextures[1] = bundle.LoadAsset<Texture2D>("Assets/shaders 1.9.03/bayer4tile8.png");
+            BayerTextures[2] = bundle.LoadAsset<Texture2D>("Assets/shaders 1.9.03/bayer8tile4.png");
+            BayerTextures[3] = bundle.LoadAsset<Texture2D>("Assets/shaders 1.9.03/bayer16tile2.png");
+            
+            // Palettes - 4 color
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/demichrome4.png")); // 0 - order of list from here for remix menu
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/hollow4.png")); // 1
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/RetroGB4.png")); // 2
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/jojo4.png")); // 3
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/amber4.png")); // 4
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/blood4.png")); // 5
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/horror4.png")); // 6
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/lava4.png")); // 7
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/miku4.png")); // 8
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/aqua4.png")); // 9
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/wish4.png")); // 10
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/moonlight4.png")); // 11
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/4color/royal4.png")); // 12
+            
+            // Palettes - 8 color
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/ammo8.png")); // 13
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/bi8.png")); // 14
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/borkfest8.png")); // 15
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/citrink8.png")); // 16
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/dream8.png")); // 17
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/fox8.png")); // 18
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/gothic8.png")); // 19
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/lava8.png")); // 20
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/morning8.png")); // 21
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/nebula8.png")); // 22
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/ocean8.png")); // 23
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/paper8.png")); // 24
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/parchment8.png")); // 25
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/purple8.png")); // 26
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/RetroGB8.png")); // 27
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/rustgold8.png")); // 28
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/submerchimera8.png")); // 29
+            palettes.Add(bundle.LoadAsset<Texture2D>("Assets/palettes/8color/winter8.png")); // 30
+            
+            // Shaders
+            GameShader = bundle.LoadAsset<Shader>("Assets/shaders 1.9.03/GameGirlScreen.shader"); // Loads the shader itself from the asset bundle
             CRTShader = bundle.LoadAsset<Shader>("Assets/shaders 1.9.03/CRTScreen.shader"); // Loads the shader itself from the asset bundle
             Camera.main.gameObject.AddComponent<CrtScreen>();
         }
@@ -81,27 +133,64 @@ sealed class Plugin : BaseUnityPlugin
 sealed class CrtScreen : MonoBehaviour
 {
     private static bool menuLoaded = false;
-    public Material mat = new Material(Plugin.CRTShader);
+    
+    public Material gameMat = new Material(Plugin.GameShader);
+    [Range(-1, 1)] public float brightness = 0.3f;
+    [Range(0, 1)] public float offset = 0.25f;
+    [Range(0, 1)] public float contrast = 0.8f;
+    private bool usePal = false;
+    
+    
+    public Material crtMat = new Material(Plugin.CRTShader);
     [Range(0, 1)] public float verts_force = 0.51f;
     [Range(0, 1)] public float verts_force_2 = 0.255f;
     [Range(0, 1)] public float verts_force_3 = 0.8f;
     [Range(0, 1)] public float screen_dist = 1.0f;
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        mat.SetFloat("_VertsColor", 1-verts_force);
-        mat.SetFloat("_VertsColor2", 1-verts_force_2);
-        mat.SetFloat("_VertsColor3", 1-verts_force_3);
-        mat.SetFloat("_ScreenDist", screen_dist); // No distortion at 0.0, screen edge dist at 1.0
-        Graphics.Blit(src, dest, mat);
+        if (usePal)
+        {
+            RenderTexture gameTemp = RenderTexture.GetTemporary(src.descriptor);
+            gameMat.SetTexture("_BayerTex", Plugin.bayerBlock.GetTexture("_BayerTex"));
+            gameMat.SetTexture("_BayPal", Plugin.bayerBlock.GetTexture("_BayPal"));
+            gameMat.SetFloat("_Brightness", brightness);
+            gameMat.SetFloat("_Offset", offset);
+            gameMat.SetFloat("_Contrast", contrast);
+            Graphics.Blit(src, gameTemp, gameMat);
+
+            crtMat.SetFloat("_VertsColor", 1 - verts_force);
+            crtMat.SetFloat("_VertsColor2", 1 - verts_force_2);
+            crtMat.SetFloat("_VertsColor3", 1 - verts_force_3);
+            crtMat.SetFloat("_ScreenDist", screen_dist); // No distortion at 0.0, screen edge dist at 1.0
+            Graphics.Blit(gameTemp, dest, crtMat);
+
+            RenderTexture.ReleaseTemporary(gameTemp);
+        }
+        else
+        {
+            crtMat.SetFloat("_VertsColor", 1 - verts_force);
+            crtMat.SetFloat("_VertsColor2", 1 - verts_force_2);
+            crtMat.SetFloat("_VertsColor3", 1 - verts_force_3);
+            crtMat.SetFloat("_ScreenDist", screen_dist); // No distortion at 0.0, screen edge dist at 1.0
+            Graphics.Blit(src, dest, crtMat);
+        }
     }
 
     private void Update()
     {
         if (menuLoaded)
         {
+            Plugin.bayerBlock.SetTexture("_BayerTex", GetDither(CRTOptions.ditherF));
+            Plugin.bayerBlock.SetTexture("_BayPal", GetPalette(CRTOptions.paletteF));
+            
             verts_force_3 = CRTOptions.distortionF / 100f;
             verts_force_2 = CRTOptions.intensityF / 100f;
             verts_force = CRTOptions.scanLineDarknessF / 100f;
+            brightness = CRTOptions.brightnessF / 100f * 2f - 1;
+            contrast = CRTOptions.contrastF / 100f;
+            offset = CRTOptions.offsetF / 100f;
+            usePal = CRTOptions.usePalF;
+            
             if (CRTOptions.screenDistF)
                 screen_dist = 1.0f;
             else
@@ -111,13 +200,21 @@ sealed class CrtScreen : MonoBehaviour
         }
         else
         {
-            if (CRTOptions.intensityF != 0f)
+            if (CRTOptions.intensityF != 0f || CRTOptions.distortionF != 0f || CRTOptions.scanLineDarknessF != 0f || CRTOptions.brightnessF != 0f || CRTOptions.offsetF != 0f || CRTOptions.contrastF != 0f)
                 menuLoaded = true;
             else
             {
+                Plugin.bayerBlock.SetTexture("_BayerTex", GetDither(CRTOptions.dither.Value));
+                Plugin.bayerBlock.SetTexture("_BayPal", GetPalette(CRTOptions.palette.Value));
+                
                 verts_force_3 = CRTOptions.distortion.Value / 100f;
                 verts_force_2 = CRTOptions.intensity.Value / 100f;
                 verts_force = CRTOptions.scanLineDarkness.Value / 100f;
+                brightness = CRTOptions.brightness.Value / 100f * 2f - 1;
+                contrast = CRTOptions.contrast.Value / 100f;
+                offset = CRTOptions.offset.Value / 100f;
+                usePal = CRTOptions.usePal.Value;
+                
                 if (CRTOptions.screenDist.Value)
                     screen_dist = 1.0f;
                 else
@@ -128,8 +225,141 @@ sealed class CrtScreen : MonoBehaviour
         }
     }
 
-    public float GetDistortion()
+    public Texture2D GetDither(string dither)
     {
-        return verts_force_3;
+        Texture2D ditherTex;
+
+        switch (dither)
+        {
+            case "Bayer2":
+                ditherTex = Plugin.BayerTextures[0];
+                break;
+            case "Bayer4":
+                ditherTex = Plugin.BayerTextures[1];
+                break;
+            case "Bayer8":
+                ditherTex = Plugin.BayerTextures[2];
+                break;
+            default:
+                ditherTex = Plugin.BayerTextures[3];
+                break;
+        }
+        return ditherTex;
     }
+
+    public Texture2D GetPalette(string palette)
+    {
+        Texture2D paletteTex;
+
+        if (Plugin.palettes.Count == Plugin.palNum)
+        {
+            switch (palette)
+            {
+                case "Chrome4":
+                    paletteTex = Plugin.palettes[0];
+                    break;
+                case "Hollow4":
+                    paletteTex = Plugin.palettes[1];
+                    break;
+                case "RetroGB4":
+                    paletteTex = Plugin.palettes[2];
+                    break;
+                case "Jojo4":
+                    paletteTex = Plugin.palettes[3];
+                    break;
+                case "Amber4":
+                    paletteTex = Plugin.palettes[4];
+                    break;
+                case "Blood4":
+                    paletteTex = Plugin.palettes[5];
+                    break;
+                case "Horror4":
+                    paletteTex = Plugin.palettes[6];
+                    break;
+                case "Lava4":
+                    paletteTex = Plugin.palettes[7];
+                    break;
+                case "Miku4":
+                    paletteTex = Plugin.palettes[8];
+                    break;
+                case "Aqua4":
+                    paletteTex = Plugin.palettes[9];
+                    break;
+                case "Wish4":
+                    paletteTex = Plugin.palettes[10];
+                    break;
+                case "Moonlight4":
+                    paletteTex = Plugin.palettes[11];
+                    break;
+                case "Royal4":
+                    paletteTex = Plugin.palettes[12];
+                    break;
+                case "Ammo8":
+                    paletteTex = Plugin.palettes[13];
+                    break;
+                case "Bi8":
+                    paletteTex = Plugin.palettes[14];
+                    break;
+                case "Borkfest8":
+                    paletteTex = Plugin.palettes[15];
+                    break;
+                case "Citrink8":
+                    paletteTex = Plugin.palettes[16];
+                    break;
+                case "Dream8":
+                    paletteTex = Plugin.palettes[17];
+                    break;
+                case "Fox8":
+                    paletteTex = Plugin.palettes[18];
+                    break;
+                case "Gothic8":
+                    paletteTex = Plugin.palettes[19];
+                    break;
+                case "Lava8":
+                    paletteTex = Plugin.palettes[20];
+                    break;
+                case "Morning8":
+                    paletteTex = Plugin.palettes[21];
+                    break;
+                case "Nebula8":
+                    paletteTex = Plugin.palettes[22];
+                    break;
+                case "Ocean8":
+                    paletteTex = Plugin.palettes[23];
+                    break;
+                case "Paper8":
+                    paletteTex = Plugin.palettes[24];
+                    break;
+                case "Parchment8":
+                    paletteTex = Plugin.palettes[25];
+                    break;
+                case "Purple8":
+                    paletteTex = Plugin.palettes[26];
+                    break;
+                case "RetroGB8":
+                    paletteTex = Plugin.palettes[27];
+                    break;
+                case "RustGold8":
+                    paletteTex = Plugin.palettes[28];
+                    break;
+                case "Chimera8":
+                    paletteTex = Plugin.palettes[29];
+                    break;
+                case "Winter8":
+                    paletteTex = Plugin.palettes[30];
+                    break;
+                default:
+                    paletteTex = Plugin.palettes[0];
+                    break;
+            }
+
+            return paletteTex;
+        }
+
+
+        return Plugin.palettes[0];
+
+    }
+
+
 }
